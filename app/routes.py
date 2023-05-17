a43e30a676f75906558ffd23e1d7ca4b33fe54e0
Nam <youngboy111999@gmail.com>
2023-05-17 15:17:49 +0700

    Chore: upadte

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,5 @@
-from app import app, db, queue_client
+# from app import app, db, queue_client
+from app import app
 from datetime import datetime
 from app.models import Attendee, Conference, Notification
 from flask import render_template, session, request, redirect, url_for, flash, make_response, session

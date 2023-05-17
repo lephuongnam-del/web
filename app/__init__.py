1b3553e252a4c474377ab16d6fd093de11332b60
Nam <youngboy111999@gmail.com>
2023-05-17 15:51:38 +0700

    Chore: upadate

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-import os
+# import os
 from flask import Flask, render_template, url_for, request, redirect
 # from flask_sqlalchemy import SQLAlchemy 
 # from azure.servicebus import QueueClient
--- application.py
+++ application.py
@@ -2,5 +2,12 @@ import os
 from app import app
 
 
+
+# running app
+def main():
+    print(' ----->>>> Flask Python Application running in development server')
+    app.run(host='0.0.0.0', port=5000, debug=True)
+
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    main()
